86cbdddb6c0ad817fafeb604a2e87d00c7a1ab11
Keloc13 <kvnyn10@gmail.com>
2017-06-27 21:14:26 -0700

    second commit: editing previous mistake of not adding in the code

---
+++ app/src/main/java/comp_sci_squad/com/github/url_irl/MainActivity.java
@@ -0,0 +1,31 @@
+package comp_sci_squad.com.github.url_irl;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.InputStream;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // This will take a picture from the resources raw in the APK and convert it to a bitmap
+        // which the TextRecognizer can reads to create a SparseArray of TextBlock
+        InputStream stream = getResources().openRawResource(R.raw.android_test_large);
+        Bitmap bitmap = BitmapFactory.decodeStream(stream);
+        TextBlockView overlay = (TextBlockView) findViewById(R.id.faceView);
+
+        String[] allText = ImageToString.getTextFromPage(this, overlay, bitmap);
+
+        // Start the intent to get a List of Strings from the image
+        Intent i = new RetrieveList().newIntent(MainActivity.this, allText);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/comp_sci_squad/com/github/url_irl/URLParsing.java
@@ -0,0 +1,8 @@
+package comp_sci_squad.com.github.url_irl;
+
+/**
+ * Created by brentfred on 6/4/17.
+ */
+
+public class URLParsing {
+}
