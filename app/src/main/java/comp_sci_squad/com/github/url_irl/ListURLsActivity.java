1d760bc93b0ac080c0e6dcbd704c8815a1597a47
HBTechnoDude <brennantracy4@gmail.com>
2017-07-01 17:51:34 -0700

    Merged URLParseTask as a subclass of ListURLsActivity

--- app/src/main/java/comp_sci_squad/com/github/url_irl/ListURLsActivity.java
+++ app/src/main/java/comp_sci_squad/com/github/url_irl/ListURLsActivity.java
@@ -3,31 +3,28 @@ package comp_sci_squad.com.github.url_irl;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ProgressBar;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ListURLsActivity extends AppCompatActivity implements UriAdapter.ListItemClickListener {
 
-    // TODO: 6/23/17 add loading bar, spinning shit, to be able to be uesd 
-    // TODO: 6/23/17 listview -> clickable, list adapter view
-    // TODO: 6/23/17 how to access uri thats in listview to open
     // TODO: 6/23/17 to do title bar, menus
     // TODO: 6/23/17 do polish
 
     private static final String URI_ARRAY_LIST = "comp_sci_squad.com.github.url_irl.uri_array_true";
 
     private UriAdapter mAdapter;
-    private RecyclerView mList;
-    private ArrayList<String> mUriList;
+    private RecyclerView recyclerView;
+    private ArrayList<String> mStringBlocks;
     private ProgressBar mLoadingIndicator;
 
     @Override
@@ -35,17 +32,20 @@ public class ListURLsActivity extends AppCompatActivity implements UriAdapter.Li
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_urls);
 
-        mUriList = getIntent().getStringArrayListExtra(URI_ARRAY_LIST);
+        mStringBlocks = getIntent().getStringArrayListExtra(URI_ARRAY_LIST);
 
-        mList = (RecyclerView) findViewById(R.id.rv_id);
+        recyclerView = (RecyclerView) findViewById(R.id.rv_id);
         mLoadingIndicator = (ProgressBar) findViewById(R.id.loading_indicator);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        mList.setLayoutManager(layoutManager);
-        mList.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
 
         mAdapter = new UriAdapter(this);
-        mList.setAdapter(mAdapter);
+        recyclerView.setAdapter(mAdapter);
+
+        //Converts arraylist<string> to var args and runs the async task
+       new UrlParseTask().execute(mStringBlocks.toArray(new String[mStringBlocks.size()]));
     }
 
     public static Intent newIntent(Context packageContext, ArrayList<String> stringList) {
@@ -60,4 +60,60 @@ public class ListURLsActivity extends AppCompatActivity implements UriAdapter.Li
         Intent i = new Intent(Intent.ACTION_VIEW, mAdapter.getUri(clickedItemIndex));
         startActivity(i);
     }
+
+    /**
+     * @author Brent Frederisy & Brennan Tracy
+     */
+    public class UrlParseTask extends AsyncTask<String, Void, ArrayList<Uri>> {
+
+        @Override
+        protected void onPreExecute() {
+            if (!isCancelled()) {
+                mLoadingIndicator.setVisibility(View.VISIBLE);
+            }
+        }
+
+        /**
+         *   Returns list of urls from a body of text.
+         *
+         *   As a precondition: a url in the text MUST have spaces before and after it.
+         *   as URLs rules allow very diverse strings.
+         *
+         *   @param textBlocks the blocks of text to parse for urls.
+         *   @return ArrayList<String> List of urls.
+         */
+        @Override
+        protected ArrayList<Uri> doInBackground(String... textBlocks) {
+            ArrayList<Uri> urls = new ArrayList<>();
+
+            String url_regex = getString(R.string.url_regex);
+            Pattern urlPattern = Pattern.compile(url_regex);
+
+            Matcher urlMatcher;
+            String temp;
+
+            for (int i = 0; i < textBlocks.length && !isCancelled(); i++)
+            {
+                urlMatcher = urlPattern.matcher(textBlocks[i]);
+                while (urlMatcher.find())
+                {
+                    temp = urlMatcher.group();
+                    if (!temp.startsWith("http://") && !temp.startsWith("https://"))
+                        temp = "http://" + temp;
+                    urls.add(Uri.parse(temp));
+                }
+            }
+
+            return urls;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Uri> urls) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            mAdapter.setArray(urls);
+        }
+
+        // TODO: 6/23/2017 decide if onCancel needs to be overrided.
+    }
+
 }
